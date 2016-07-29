ae923988ecab078595d4f58bccd6edd6f91e026a
Anish Ramasekar <aramase@users.noreply.github.com>
2016-07-29 15:49:05 -0400

    Update Peer2.java

--- Peer2.java
+++ Peer2.java
@@ -9,10 +9,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-/**
- *
- * @author Lakshminarayan
- */
 public class Peer2{
     public static void main(String[] args) throws IOException
     {
